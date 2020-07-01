c0d9ff7b6eac2c0abea8874c41cc04e34b819322
Thabiso Pheko <tabs150@gmail.com>
2020-07-01 12:56:30 +0200

    Hero section

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,7 @@ const useStyles = makeStyles((theme) => ({
   toolbarLink: {
     padding: theme.spacing(1),
     flexShrink: 0,
+    textTransform: 'uppercase',
   },
 }));
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import ProductCard from './ProductCard';
+import Hero from './Hero';
 import { makeStyles } from '@material-ui/core/styles';
+
+const heroPost = {
+  title: 'Title of a longer featured blog post',
+  description:
+    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
+  image: 'https://source.unsplash.com/random',
+  imgText: 'main image description',
+  linkText: 'Continue reading…',
+};
 
 const useStyles = makeStyles((theme) => ({
   grid: {
@@ -13,32 +23,35 @@ const useStyles = makeStyles((theme) => ({
 const Home = () => {
   const classes = useStyles();
   return (
-    <Grid container spacing={2} className={classes.grid}>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
+    <>
+      <Hero post={heroPost} />
+      <Grid container spacing={2} className={classes.grid}>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
+        <Grid item xs={6} sm={3} align='center'>
+          <ProductCard />
+        </Grid>
       </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-      <Grid item xs={6} sm={3} align='center'>
-        <ProductCard />
-      </Grid>
-    </Grid>
+    </>
   );
 };
 
